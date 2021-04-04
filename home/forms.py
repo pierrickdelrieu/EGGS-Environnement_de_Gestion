651c190456de4658637602bc4199ee1d1048ef86
pierrickdelrieu <pierrick.delrieu@efrei.net>
2021-04-04 09:36:04 +0200

    ajout des validations de formulaire et des conditions de mot de passe personnalisés

--- home/forms.py
+++ home/forms.py
@@ -83,6 +83,18 @@ class PasswordResetConfirmationForm(SetPasswordForm):
         widget=forms.PasswordInput,
     )
 
+    def clean_new_password2(self):
+        password1 = self.cleaned_data.get('new_password1')
+        password2 = self.cleaned_data.get('new_password2')
+        if password1 and password2:
+            if password1 != password2:
+                raise ValidationError(
+                    _('Les deux mot de passe sont différents'),
+                    code='password_mismatch',
+                )
+        password_validation.validate_password(password2, self.user)
+        return password2
+
 
 # Redefinition
 # cf. : https://django-wiki.readthedocs.io/en/0.2.2/_modules/django/contrib/auth/forms.html
